02623a1a9f8a525cae2cc93de4abc51a0f57875e
wobozkyng <42249669+wobozkyng@users.noreply.github.com>
2023-05-08 18:01:31 +0700

    tweak: utilize ox_lib points

--- client/cl_function.lua
+++ client/cl_function.lua
@@ -8,3 +8,33 @@ function labelText(text, ...)
     end
     return library[text]:format(...) 
 end
+
+function claimStarterPack()
+    if not Config.available then
+        return ESX.ShowNotification(labelText("claimed"), "error")
+    end
+    local plate = exports["esx_vehicleshop"]:GeneratePlate()
+    local model = type(Config.vehicle?.model) == "number" and Config.vehicle?.model or joaat(Config.vehicle?.model)
+    if not IsModelInCdimage(model) then
+        return
+    end
+    if not HasModelLoaded(model) then
+        RequestModel(model)
+        while not HasModelLoaded(model) do
+            Wait(100)
+        end
+    end
+    local vehicle = CreateVehicle(model, Config.vehicle?.coords, Config.vehicle?.heading, false, false)
+    SetVehicleNumberPlateText(vehicle, plate)
+    local props = ESX.Game.GetVehicleProperties(vehicle)
+    DeleteEntity(vehicle)    
+    local carname = GetDisplayNameFromVehicleModel(model)
+    local vehicleName = GetLabelText(carname)   
+    local data = {
+        plate = plate,
+        props = props,
+        label = vehicleName
+    }
+    Config.available = false
+    TriggerServerEvent("starterpack:claim", data)
+end
